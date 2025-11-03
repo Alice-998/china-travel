6dc29b245b83425fb351104dfcf74295b7d5aebb
Alice-998 <aihongx2@gmail.com>
2025-11-03 17:33:57 +0800

    backup

--- main.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelectorAll('.item');
-
-items.forEach(item => {
-  item.addEventListener('click', () => {
-    const province = item.dataset.province;
-    window.location.href = `map.html?province=${province}`;
-  });
-});
-
